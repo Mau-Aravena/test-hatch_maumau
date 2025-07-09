8f965165e776f1bb00cd39731ea52e706a81f602
Mau-Aravena <mau.aravena.acevedo@gmail.com>
2025-07-09 00:20:19 -0400

    Add saystegosaurus function in the package.

--- src/test_hatch_maumau/__init__.py
+++ src/test_hatch_maumau/__init__.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2025-present MauMau <mau.aravena@outlook.com>
 #
 # SPDX-License-Identifier: BSD-3-Clause
-from .script1 import saycow
+from .script1 import saycow, saystegosaurus
 
-__all__ = ["saycow"]
+__all__ = ["saycow", "saystegosaurus"]
--- src/test_hatch_maumau/script1.py
+++ src/test_hatch_maumau/script1.py
@@ -28,5 +28,46 @@ def saycow(text: str):
     return cowsay.get_output_string(char="cow", text=text)
 
 
+def saystegosaurus(text: str):
+    """
+    Displays a cow-themed ASCII art message using the 'cowsay' library.
+
+    Parameters
+    ----------
+    text (str): The message to be shown in the speech bubble above the cow.
+
+    Returns
+    -------
+    A string of cowsay with the text
+
+    Example
+    -------
+        >>> print(saycow("Hi, :D"))
+      ______
+    | Hi, :D |
+      ======
+                \.
+                 \.
+                  \.
+                   \.
+                                      .       .
+                                     / `.   .' \.
+                             .---.  <    > <    >  .---.
+                             |    \  \ - ~ ~ - /  /    |
+                 _____        ~-..-~             ~-..-~
+                |     |   \~~~\.'                    `./~~~/
+               ---------   \__/                        \__/
+              .'  O    \     /               /       \  "
+             (_____,    `._.'               |         }  \/~~~/
+              `----.          /       }     |        /    \__/
+                    `-.      |       /      |       /      `. ,~~|
+                        ~-.__|      /_ - ~ ^|      /- _      `..-'   f:  f:
+                             |     /        |     /     ~-.     `-. _|| _||_
+                             |_____|        |_____|         ~ - . _ _ _ _ __>
+    """
+    return cowsay.get_output_string(char="stegosaurus", text=text)
+
+
 if __name__ == "__main__":
-    print(saycow("perro"))
+    print(saycow("Dog"))
+    print(saystegosaurus("Dog"))
